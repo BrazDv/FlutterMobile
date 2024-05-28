0cb91f9e8b6358f130180a7f75231c9f8121c9e3
unknown <jbraz63@gmail.com>
2024-05-27 21:06:22 -0300

    Alterações

--- lib/main.dart
+++ lib/main.dart
@@ -18,31 +18,55 @@ class MyApp extends StatelessWidget {
 class HomePage extends StatelessWidget {
   const HomePage({Key? key}) : super(key: key);
 
+  void decrement() {
+    // Defina aqui a lógica para decrementar
+    print('Decrement pressed');
+  }
+
+  void increment() {
+    // Defina aqui a lógica para incrementar
+    print('Increment pressed');
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.black,
       body: Column(
         mainAxisAlignment: MainAxisAlignment.center,
-        children: const [
-          Text(
-            'Page Entrar!',
+        children: [
+          const Text(
+            'Pode Entrar!',
             style: TextStyle(
               fontSize: 30,
               color: Colors.white,
               fontWeight: FontWeight.w800,
             ),
           ),
-          Text(
+          const Text(
             '0',
             style: TextStyle(
               fontSize: 100,
               color: Colors.white,
             ),
           ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              TextButton(
+                onPressed: decrement,
+                child: const Text('Saiu'),
+                style: TextButton.styleFrom(),
+              ),
+              TextButton(
+                onPressed: increment,
+                child: const Text('Entrou'),
+                style: TextButton.styleFrom(),
+              ),
+            ],
+          ),
         ],
       ),
     );
   }
 }
--=
